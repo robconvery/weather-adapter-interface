1eeeba4fce6d6824bbb3da3dbb116b1171814ede
Robert Convery <robconvery@me.com>
2018-12-16 16:42:43 +0000

    Created interface with methods

---
+++ src/WeatherAdapterInterface.php
@@ -0,0 +1,52 @@
+<?php declare(strict_types=1);
+/**
+ * Interface WeatherAdapterInterface
+ */
+
+namespace Robertconvery\WeatherAdapterInterface;
+
+use Carbon\Carbon;
+
+interface WeatherAdapterInterface
+{
+    /**
+     * WeatherAdapterInterface constructor.
+     * @param array $data
+     */
+    public function __construct(array $data);
+
+    /**
+     * @return string
+     */
+    public function location(): string;
+
+    /**
+     * @return Carbon
+     */
+    public function datetime(): Carbon;
+
+    /**
+     * @return string
+     */
+    public function main(): string;
+
+    /**
+     * @return string
+     */
+    public function description(): string;
+
+    /**
+     * @return int
+     */
+    public function temperature(): int;
+
+    /**
+     * @return int
+     */
+    public function temperatureMin(): int;
+
+    /**
+     * @return int
+     */
+    public function temperatureMax(): int;
+}
