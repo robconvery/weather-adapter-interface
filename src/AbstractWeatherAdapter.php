357d8de3aeee519e885319a3904a392285f984e9
Robert Convery <robconvery@me.com>
2018-12-17 09:08:12 +0000

    created abstract class

---
+++ src/AbstractWeatherAdapter.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * Class AbstractWeatherAdapter
+ *
+ * @package Robertconvery\WeatherAdapter
+ * @author robconvery <robconvery@me.com>
+ */
+
+namespace Robertconvery\WeatherAdapter;
+
+abstract class AbstractWeatherAdapter
+{
+
+    /**
+     * Append a forecast to the list of forecasts
+     * @param AbstractWeatherAdapter $adapter
+     * @return mixed
+     */
+    abstract public function attach(AbstractWeatherAdapter $adapter);
+
+    /**
+     * @return Collection
+     */
+    abstract public function forecasts(): Collection;
+
+    /**
+     * @return string
+     */
+    abstract public function location(): string;
+
+    /**
+     * @return Carbon
+     */
+    abstract public function datetime(): Carbon;
+
+    /**
+     * @return string
+     */
+    abstract public function main(): string;
+
+    /**
+     * @return string
+     */
+    abstract public function description(): string;
+
+    /**
+     * @return int
+     */
+    abstract public function temperature(): int;
+
+    /**
+     * @return int
+     */
+    abstract public function temperatureMin(): int;
+
+    /**
+     * @return int
+     */
+    abstract public function temperatureMax(): int;
+}
